b3eaf5225a4bad295e1b0a34dc0b7a13aebceb6d
Abdul <abdulwaheed1513@gmail.com>
2018-08-17 17:41:05 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-bool binarySearchRecursive(int[], int mid, int left, int right, int target);
+bool binarySearchRecursive(int[], int start, int end, int find);
 int main(){
 	int arr[] = { 23, 44, 67, 43, 12, 69, 41, 27, 90, 123 };
 	int size = sizeof(arr)/sizeof(*arr);
-	
-	sort(arr, arr+10);
-	
+	sort(arr, arr + size);
 	// 1- true 
-	// 0 -false
-	cout <<  binarySearchRecursive(arr, size/2, 0,size-1, 12) << "\n";	
+	// 0-false
+	cout <<  binarySearchRecursive(arr, 0, size-1, 1) << "\n";	
 	return 0;
 }
-
-bool binarySearchRecursive(int arr[], int mid, int left, int right, int target){
-	
-	mid = ( left + right ) / 2;
-	if (left  > right){
+bool binarySearchRecursive(int arr[], int start, int end, int find){
+	int mid = ( start + end ) / 2;
+	if (start  > end){
 		return false;
 	}
-	if(arr[mid] == target){
+	if(arr[mid] == find){
 		return true;
 	}
-	else if(target > arr[mid]){
-		binarySearchRecursive(arr, mid, mid+1,right, target);
+	else if(find > arr[mid]){
+		binarySearchRecursive(arr, mid+1, end, find);
 	}
 	else{
-		binarySearchRecursive(arr, mid, left, mid-1, target);
+		binarySearchRecursive(arr, start, mid-1, find);
 	}
-
 }
-	
