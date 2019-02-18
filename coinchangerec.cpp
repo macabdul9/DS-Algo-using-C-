a7d67482933e256278ac7790d4a3fcf12e77142d
macabdul9 <abdulwaheed1513@gmail.com>
2019-02-18 20:53:55 +0530

    via terminal

--- coinchangerec.cpp
+++ coinchangerec.cpp
@@ -35,37 +35,37 @@ using namespace std;
 mii mm;
 
 int coinchange(int amount, vi coins){
-    if(!amount) return 1; //no amount left
-    if(amount < 0) return 0; //
+    if(amount == 0) return 0; //no amount left
 
-    //return coinchange(amount - 10) + coinchange(amount - 7) + coinchange(amount - 1);
-    for(int i = 0; i < coins.size(); i++){
-        int current = coinchange(amount - coins[i], coins);
-        if(!current)
-                continue;
-        else if(current == 1)
-                mm.insert(pii(amount, current));
-        else if(mm.find(amount - coins[i]) != mm.end()){
-                mm.insert(pii(amount, 1 + mm.at(amount - coins[i])));
-        }else{
-            coinchange(amount - coins[i], coins);
+    int min, i;
+    min = amount;
+    for(auto coin : coins){
+        if(amount - coin >= 0){
+            int c = coinchange(amount - coin, coins);
+            //min = c + 1 ? min > c + 1 : min;
+            if(min > c + 1)
+                    min = c + 1;
+            //cout << min << endl;
         }
+
     }
-    return mm.at(amount)->second;
+    return min;
 }
 
 int main(){
     /*code goes here*/
     vector<int> coins;
-    //coins.push_back(10);
     coins = {2, 7, 10};
-    //cout << coins.size() << endl;
-    coinchange(14, coins);
-    cout << mm.at(14) << endl;
-    //mii::iterator itr;
-    //cout << "key\tvalue" << endl;
-    //for(itr =  mm.begin(); itr != mm.end(); itr++)
-    //        cout << itr -> first << "\t" << itr -> second << endl;
+    cout << coinchange(81, coins) << endl;
+
+
+
+    /*
+    mii::iterator itr;
+    cout << "key\tvalue" << endl;
+    for(itr =  mm.begin(); itr != mm.end(); itr++)
+            cout << itr -> first << "\t" << itr -> second << endl;
+    */
 
     return 0;
 }
