b27f77a2f2ef15930f46f019a7710cf36a37e2c5
macabdul9 <abdulwaheed1513@gmail.com>
2019-02-26 03:52:29 +0530

    via terminal

---
+++ laddermaxjump.cpp
@@ -0,0 +1,51 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : laddermaxjump
+ * @created   : Tuesday Feb 26, 2019 03:39:52 IST
+*/
+#include<bits/stdc++.h>
+#define endl 		         "\n"
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define mii                   map<int, int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+using namespace std;
+
+/*
+ * description : total ways if max possible jump is given */
+int ways(int steps, int maxj){
+    if(!steps) return 1;
+    if(steps < 0) return 0;
+    int ans = 0;
+    rloop(i, 1, maxj)
+            ans += ways(steps - i, maxj);
+
+    return ans;
+}
+int main(){
+    /*code goes here*/
+    cout << ways(4, 4);
+    return 0;
+}
+
