17b1165915f6b27124815b43f665402a9b601e00
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-13 11:12:42 +0530

    via terminal

--- square.cpp
+++ square.cpp
@@ -1,15 +1,15 @@
 /*
  * @author    : macab (macab@debian)
- * @file      : towerofhanoi
- * @created   : Monday Feb 11, 2019 06:33:17 IST
+ * @file      : square
+ * @created   : Tuesday Mar 12, 2019 16:35:28 IST
 */
 #include<bits/stdc++.h>
-#define endl 		         "\n"
+#define endl                  "\n"
+#define merge(a, b)           a##b
 #define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
 #define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
 #define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
 #define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
-#define clr(x)                x.clear()
 #define MOD                   1000000007
 #define MAX                   1e9
 #define MIN                   -1e9
@@ -21,33 +21,39 @@
 #define max(x,y)              (x>y)?x:y
 #define min(x,y)              (x<y)?x:y
 #define vi                    vector<int>
+#define vll                   vector<long long int>
 #define vs                    vector<string>
 #define si                    set<int>
 #define ss                    set<string>
+#define sll                   set<long long int>
 #define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
 typedef long long int ll;
 typedef unsigned int uint;
+typedef unsigned long long int ull;
 using namespace std;
 
-int towerofhanoi(int disk, char source, char aux, char destination){
-    if(disk <= 0) return 0;
-    if(disk == 1){
-        cout << "move " << source << " to " << destination << endl;
-        return 1;
-    }
-int a = 0, b = 0, c  = 0;
-    a += towerofhanoi(disk - 1, source, destination, aux);
-    c++;
-    cout << "move " << source << " to " << destination << endl;
-    b += towerofhanoi(disk - 1, aux, source, destination);
 
-    return a + b + c;
+int sqaure(int num){
+    if(num == 1) return 1;
 
+    return (2*num - 1) + sqaure(num - 1);
 }
 
 int main(){
-    /*code goes here */
-    cout << towerofhanoi(6, 'A', 'B', 'C');
-    return 0;
+	ios::sync_with_stdio(0);
+
+	/*your code goes here*/
+    int n;
+    while(true){
+        cin >> n;
+        cout << "square of "<< n <<" by sqaure fun = " << sqaure(n) << endl ;
+        cout << "square of "<< n <<" by power fun  = " << int(pow(n, 2)) << endl;
+    }
+
+	return 0;
 }
 
