3c1bfd58e92af093f8330b9ee111a8febddbca74
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-21 17:48:34 +0530

    counting the set bits

---
+++ bitmasking/changingbits.cpp
@@ -0,0 +1,79 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : changingbit
+ * @created   : Thursday Mar 21, 2019 13:20:24 IST
+*/
+#include<bits/stdc++.h>
+#define endl                  "\n"
+#define merge(a, b)           a##b
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vll                   vector<long long int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define sll                   set<long long int>
+#define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+using namespace std;
+
+/*
+ * two numbers are a and b, how many bit we need to change to make a == b ?
+ */
+
+
+// count the set bits in XOR of two number
+int countSetBits(int num){
+
+    int count = 0;
+    while(num > 0){
+        count += num&1;
+        num = num >> 1;
+    }
+    return count;
+
+
+}
+
+
+
+int main(){
+
+	ios::sync_with_stdio(0);
+
+    int num1 , num2, XOR;
+
+
+    while(true){
+
+        cin >> num1 >> num2;
+
+        XOR = num1^num2;
+
+        cout << countSetBits(XOR) << endl;
+    }
+
+
+
+	return 0;
+}
+
