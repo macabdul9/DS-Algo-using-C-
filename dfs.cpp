09fc03fc5fccf5df366c53b61fffeec233c814cb
macabdul9 <abdulwaheed1513@gmail.com>
2019-04-27 22:37:30 +0530

    add files via terminal

---
+++ dfs.cpp
@@ -0,0 +1,82 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : dfs
+ * @created   : Saturday Apr 27, 2019 21:52:26 IST
+*/
+#include<bits/stdc++.h>
+#define endl                  "\n"
+#define merge(a, b)           a##b
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vll                   vector<long long int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define sll                   set<long long int>
+#define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+using namespace std;
+
+class Graph{
+    list<int> *adjList;
+    int V;
+    public:
+        Graph(int v){
+            this->V = v;
+            adjList = new list<int>[V];
+        }
+
+        void addEdge(int u, int v, bool isBiDir = true){
+            adjList[u].push_back(v);
+            if(isBiDir)
+                    adjList[v].push_back(u);
+        }
+
+        void bfs(int source, vector<bool> &visited){
+            cout << source << " ";
+            visited[source] = true;
+            for(auto neighbour : adjList[source]){
+                if(!visited[neighbour])
+                        bfs(neighbour, visited);
+            }
+        }
+};
+
+int main(){
+	ios::sync_with_stdio(0);
+
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    vector<bool> visited(4, false);
+
+    g.bfs(2, visited);
+
+
+	return 0;
+}
+
