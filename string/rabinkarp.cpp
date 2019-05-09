546aff31d0c71a88f56cf1f3b87de979aed42d1e
macabdul9 <abdulwaheed1513@gmail.com>
2019-05-09 11:51:55 +0530

    rabin carp pattern matching algorithm

--- string/rabinkarp.cpp
+++ string/rabinkarp.cpp
@@ -36,11 +36,59 @@ typedef unsigned int uint;
 typedef unsigned long long int ull;
 using namespace std;
 
+ull hashFun(string str){
+    ull hashValue = 0;
+    int str_len = str.length();
+    loop(i, 0, str_len){
+        hashValue += (int(str[i])*pow(10, str_len - 1 - i));
+    }
+
+    return hashValue;
+}
+
+/* modify the hashValue of the string after every new char arrival ! */
+ull hashModifier(ull hash, char old, char newChar, int p_len){
+    /*
+     a*10^2 + b*10^1 + c*10^0
+     supposed new char has arrived is d then has value will be:
+     b*10^2 + c*10^1 + d*10^0
+    */
+
+    return (hash - int(old)*pow(10, p_len - 1))*10 + int(newChar);
+
+}
+
+// time complexity of this function in worst case will be  some O(n - m + 1) where n is length of string and m is length of pattern !
+
+int rabinkarp(string str, string pattern){
+    int s_len, p_len;
+    s_len = str.length();
+    p_len = pattern.length();
+
+    if(s_len < p_len) return -1;
+
+    ull patternHash, currentHash;
+
+    patternHash = hashFun(pattern);
+
+    currentHash = hashFun(str.substr(0, p_len));
+
+    if(currentHash == patternHash) return 0;
+
+    for(int i = 0; i <= s_len - p_len; i++){
+        if(currentHash == patternHash)
+                return i; // start index;
+        else
+                currentHash = hashModifier(currentHash, str[i], str[i + p_len], p_len);
+    }
+
+    return -1;
+}
+
 int main(){
 	ios::sync_with_stdio(0);
 
-	
-
+    cout << rabinkarp("ccaaccaaedba", "ba") << endl;
 
 	return 0;
 }
