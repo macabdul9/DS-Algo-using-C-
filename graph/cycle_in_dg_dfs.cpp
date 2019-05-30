7723bfedae5089aaca2cd2e4812d07981a79d769
macabdul9 <abdulwaheed1513@gmail.com>
2019-05-31 03:17:12 +0530

    cycle in dg using dfs

---
+++ graph/cycle_in_dg_dfs.cpp
@@ -0,0 +1,129 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : cycle_in_directed_graph
+ * @created   : Saturday Apr 27, 2019 23:04:35 IST
+*/
+#include<bits/stdc++.h>
+#define endl                  "\n"
+#define merge(a, b)           a##b
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vll                   vector<long long int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define sll                   set<long long int>
+#define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+using namespace std;
+
+// source : https://www.geeksforgeeks.org/detect-cycle-undirected-graph/
+
+// Class for an undirected graph 
+class Graph 
+{ 
+    int V;    // No. of vertices 
+    list<int> *adj;    // Pointer to an array containing adjacency lists 
+    bool isCyclicUtil(int v, bool visited[], int parent); 
+public: 
+    Graph(int V);   // Constructor 
+    void addEdge(int v, int w);   // to add an edge to graph 
+    bool isCyclic();   // returns true if there is a cycle 
+}; 
+  
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list<int>[V]; 
+} 
+  
+void Graph::addEdge(int v, int w) 
+{ 
+    adj[v].push_back(w); // Add w to v’s list. 
+    adj[w].push_back(v); // Add v to w’s list. 
+} 
+  
+// A recursive function that uses visited[] and parent to detect 
+// cycle in subgraph reachable from vertex v. 
+bool Graph::isCyclicUtil(int v, bool visited[], int parent) 
+{ 
+    // Mark the current node as visited 
+    visited[v] = true; 
+  
+    // Recur for all the vertices adjacent to this vertex 
+    list<int>::iterator i; 
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) 
+    { 
+        // If an adjacent is not visited, then recur for that adjacent 
+        if (!visited[*i]) 
+        { 
+           if (isCyclicUtil(*i, visited, v)) 
+              return true; 
+        } 
+  
+        // If an adjacent is visited and not parent of current vertex, 
+        // then there is a cycle. 
+        else if (*i != parent) 
+           return true; 
+    } 
+    return false; 
+} 
+  
+// Returns true if the graph contains a cycle, else false. 
+bool Graph::isCyclic() 
+{ 
+    // Mark all the vertices as not visited and not part of recursion 
+    // stack 
+    bool *visited = new bool[V]; 
+    for (int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    // Call the recursive helper function to detect cycle in different 
+    // DFS trees 
+    for (int u = 0; u < V; u++) 
+        if (!visited[u]) // Don't recur for u if it is already visited 
+          if (isCyclicUtil(u, visited, -1)) 
+             return true; 
+  
+    return false; 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    Graph g1(5); 
+    g1.addEdge(1, 0); 
+    g1.addEdge(0, 2); 
+    g1.addEdge(2, 0); 
+    g1.addEdge(0, 3); 
+    g1.addEdge(3, 4); 
+    g1.isCyclic()? cout << "Graph contains cycle\n": 
+                   cout << "Graph doesn't contain cycle\n"; 
+  
+    Graph g2(3); 
+    g2.addEdge(0, 1); 
+    g2.addEdge(1, 2); 
+    g2.isCyclic()? cout << "Graph contains cycle\n": 
+                   cout << "Graph doesn't contain cycle\n"; 
+  
+    return 0; 
+} 
